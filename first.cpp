665b2191d99567d3d12f237b81468cc862974801
honestcodee <dr.faizi001@gmail.com>
2024-09-21 03:27:25 -0700

    Update and rename practice.cpp to first.cpp

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main{
+    string name;
+    cout<<"enter your name"<<endl;
+    cin>>name;
+    cout<<"you are very good "<<name<<" person<<endl;
+    return 0;
+}
